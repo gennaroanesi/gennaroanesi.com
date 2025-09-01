ffaa202f709273d01dc50f3403aa79670a068b36
Gennaro Anesi <gennaroanesi@meta.com>
2025-09-01 10:43:15 -0500

    try to remove user pool entirely

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,7 @@ import { defineBackend } from "@aws-amplify/backend";
 import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 
-import { auth } from "./auth/resource";
+// import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
 const backend = defineBackend({
